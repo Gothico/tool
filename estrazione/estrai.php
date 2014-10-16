7e7493c52918665cf63b9077e58fe59b7b5fb2a6
Gothico <frusciante2000@hotmail.it>
2014-10-16 10:52:34 +0200

    v 0.1.1
    aggiunta della licenza CC adeguata

--- estrazione/estrai.php
+++ estrazione/estrai.php
@@ -1,4 +1,27 @@
 <?php
+/*
+ * Tale sorgente è distribuito secondo licenza CC BY-NC-SA 4.0: 
+ * 			http://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+ * 
+ * Tu sei libero di:
+ *    - condividere — riprodurre, distribuire, comunicare al pubblico, 
+ * 		esporre in pubblico, rappresentare, eseguire e recitare 
+ * 		questo materiale con qualsiasi mezzo e formato; 
+ *    - modificare — remixare, trasformare il materiale e basarti su di 
+ * 		esso per le tue opere.
+ * 	
+ * Il licenziante non può revocare questi diritti fintanto che tu 
+ * rispetti i termini della licenza alle seguenti condizioni:
+ * 		- devi riconoscere l'autore originario e fornire un link alla 
+ * 		  licenza;
+ *      - non puoi usare il materiale per scopi commerciali;
+ *      - se modifichi, o comunque ti basi su questo materiale devi 
+ * 		  distribuire i tuoi elaborati con la stessa licenza.
+ * 			
+ * Autore: Riccardo Monterisi
+ * */
+?>
+<?php
 
          $someUA = array (
 				"Mozilla/5.0 (Windows; U; Windows NT 6.0; fr; rv:1.9.1b1) Gecko/20081007 Firefox/3.1b1",
